9dd0486fa48daf1bb913b235802caeec6e3a8995
benni043 <benni.huff@gmail.com>
2022-11-28 19:59:46 +0100

    24.10

--- homework/src/app/e8-server/e8-server.component.ts
+++ homework/src/app/e8-server/e8-server.component.ts
@@ -1,23 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-e8-server',
   templateUrl: './e8-server.component.html',
   styleUrls: ['./e8-server.component.css']
 })
-export class E8ServerComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class E8ServerComponent {
 
   servers: {name: string, type: string}[] = [];
 
   name: string = "";
-  type: string = "";
+  type: string = "TRAINING";
 
-  func() {
+  addServerToServerList() {
+    if(this.name === "") return;
     this.servers.push({name: this.name, type: this.type});
   }
 

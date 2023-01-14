95001868f7dc05e5e3f3da0c3e8b3c2770ae7885
benni043 <benni.huff@gmail.com>
2023-01-14 12:20:26 +0100

    backend homework finished 2023-01-14

--- e14-backend/src/index.ts
+++ e14-backend/src/index.ts
@@ -2,41 +2,48 @@ import {createServer, IncomingMessage, ServerResponse} from "http";
 
 const port = 3000
 const fs = require('fs');
-createServer((req: IncomingMessage, res: ServerResponse) => {
-    res.writeHead(200, {'content-type': 'text/html'})
-    res.write(fs.readFileSync("src/index.html"))
 
+let rawData = "Benedikt";
+
+createServer((req: IncomingMessage, res: ServerResponse) => {
     if (req.method == 'GET' && req.url == '/hello') {
         getHello(req, res)
-    }
-    if (req.method == 'GET' && req.url == '/goodbye') {
+    } else if (req.method == 'GET' && req.url == '/goodbye') {
         getGoodbye(req, res)
-    }
-    if (req.method == 'POST' && req.url == '/hello') {
+    } else if (req.method == 'POST' && req.url == '/hello') {
         postHello(req, res)
+    } else {
+        res.writeHead(404, {'content-type': 'text/html'})
+        res.write("404")
+        res.end()
     }
-
-    res.end()
 
 }).listen(port, () => console.log(`Server listens on ${port}`))
 
 
 function getHello(req: IncomingMessage, res: ServerResponse) {
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write(Buffer.from(fs.readFileSync("src/index.html").toString().replace("*", rawData)));
+    res.end()
 }
 
 function getGoodbye(req: IncomingMessage, res: ServerResponse) {
-    res.writeHead(301, {"Location": "/hello"})
+    rawData = "Benedikt";
+
+    res.writeHead(302, {"Location": "/hello"})
+    res.end()
 }
 
-
 function postHello(req: IncomingMessage, res: ServerResponse) {
-    let rawData = '';
+    rawData = "";
 
     req.on('data', chunk => {
         rawData += chunk
     })
     req.on('end', () => {
-        console.log(rawData)
-        res.end();
+        rawData = rawData.split("=")[1];
+
+        res.writeHead(302, {"Location": "/hello"})
+        res.end()
     })
 }

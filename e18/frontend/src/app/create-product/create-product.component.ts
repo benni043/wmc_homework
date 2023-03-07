b0b6bae6bf506538b3b7594594f704ff513b8c9b
benni <benni.huff@gmail.com>
2023-03-07 18:35:59 +0100

    initnew

---
+++ e18/frontend/src/app/create-product/create-product.component.ts
@@ -0,0 +1,29 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Product} from "../product";
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-create-product',
+  templateUrl: './create-product.component.html',
+  styleUrls: ['./create-product.component.css']
+})
+export class CreateProductComponent {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  price: number = 0;
+  name: string = "";
+
+  create() {
+    this.httpClient.post<Product>("http://localhost:7000/api/product", {name: this.name, price: this.price} as Product)
+      .subscribe(res => {
+        this.pushProduct.emit(res);
+      })
+
+  }
+
+  @Output() pushProduct: EventEmitter<Product> = new EventEmitter<Product>();
+
+}

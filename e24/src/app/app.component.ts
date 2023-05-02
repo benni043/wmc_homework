76d68a58394cc57ec9c8cea4e94bee71291de291
benni <benni.huff@gmail.com>
2023-05-02 14:08:56 +0200

    huff

---
+++ e24/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title: string = "";
+  description: string = "";
+  releaseYear: number = 0;
+  country: string = "";
+  genre: string = "";
+}
